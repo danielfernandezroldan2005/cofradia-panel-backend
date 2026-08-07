a2046e1c77777b9350fc3a967fdda63809db21d1
Daniel Fernández Roldán <danielfernandezroldan2005@gmail.com>
2026-08-07 18:45:07 +0200

    Added a new endpoint for the brother endpoint.

--- src/main/java/com/cofradia/panel/controllers/HermanoController.java
+++ src/main/java/com/cofradia/panel/controllers/HermanoController.java
@@ -3,10 +3,7 @@ package com.cofradia.panel.controllers;
 import com.cofradia.panel.models.Hermano;
 import com.cofradia.panel.repositories.HermanoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +15,23 @@ public class HermanoController {
     @Autowired
     private HermanoRepository hermanoRepository;
 
+    /**
+     * Endpoint to get all the members in the database.
+     * @return All members of the brotherhood or cofradia.
+     */
     @GetMapping
     public List<Hermano> getAllHermanos() {
         return hermanoRepository.findAll();
+    }
+
+    /**
+     * Endpoint to create and save a new member (hermano) in the database.
+     * @param newHermano The JSON payload automatically mapped to a Hermano object.
+     * @return The saved Hermano object, including the auto-generated ID.
+     */
+    @PostMapping
+    public Hermano createHermano(@RequestBody Hermano newHermano) {
+        // The repository's save() method automatically persists the entity
+        return hermanoRepository.save(newHermano);
     }
 }
